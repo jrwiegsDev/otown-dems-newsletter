382dcb41eb4c6f03eb97c2e98cd0ba87af9bac0f
Joe <jrwiegs@gmail.com>
2025-10-01 17:38:57 -0500

    fix: redirect to login page

---
+++ frontend/src/components/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+const PrivateRoute = () => {
+  const { user } = useAuth();
+
+  // If a user is logged in, show the page. Otherwise, redirect to login.
+  return user ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
