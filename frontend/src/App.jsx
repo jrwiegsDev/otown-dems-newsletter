9aa7565d338a399589c06bd333bc1645c0d615e2
Joe <jrwiegs@gmail.com>
2025-10-01 17:38:57 -0500

    fix: redirect to login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
+import PrivateRoute from './components/PrivateRoute'; // <-- 1. IMPORT
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<DashboardPage />} />
+        {/* 2. WRAP THE DASHBOARD ROUTE */}
+        <Route path="/" element={<PrivateRoute />}>
+          <Route path="/" element={<DashboardPage />} />
+        </Route>
       </Routes>
     </Router>
   );
